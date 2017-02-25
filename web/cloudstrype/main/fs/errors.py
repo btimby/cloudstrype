88a0506bf8b9a82b25230c97721fdfe5e21d919c
Ben Timby <btimby@smartfile.com>
2017-02-25 03:13:49 -0500

    Coverage.

--- web/cloudstrype/main/fs/errors.py
+++ web/cloudstrype/main/fs/errors.py
@@ -2,17 +2,37 @@ class BaseError(Exception):
     pass
 
 
-class PathNotFoundError(BaseError):
-    pass
+class PathError(BaseError):
+    def __init__(self, msg, path):
+        self.path = path
+        super().__init__(msg % path)
+
+
+class PathNotFoundError(PathError):
+    def __init__(self, path):
+        super().__init__('path "%s" does not exist', path)
 
 
 class FileNotFoundError(PathNotFoundError):
-    pass
+    def __init__(self, path):
+        super().__init__('file "%s" does not exist', path)
 
 
 class DirectoryNotFoundError(PathNotFoundError):
-    pass
+    def __init__(self, path):
+        super().__init__('directory "%s" does not exist', path)
 
 
-class DirectoryNotEmptyError(BaseError):
-    pass
+class DirectoryConflictError(PathError):
+    def __init__(self, path):
+        super().__init__('path "%s" exists as directory', path)
+
+
+class FileConflictError(PathError):
+    def __init__(self, path):
+        super().__init__('path "%s" exists as file', path)
+
+
+class DirectoryNotEmptyError(PathError):
+    def __init__(self, path):
+        super().__init__('directory "%s" not empty', path)
