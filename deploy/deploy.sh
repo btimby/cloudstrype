a08be87c4fe9ada8c4fd800a148e51f11af4f50d
Ben Timby <btimby@smartfile.com>
2017-02-26 17:56:01 -0500

    Deploy script.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,13 +8,16 @@ WEBROOT="/usr/share/nginx/cloudstrype/"
 CONFIG_NGINX="/etc/nginx/conf.d/cloudstrype.conf"
 CONFIG_SUPERVISORD="/etc/supervisord.d/cloudstrype.ini"
 
-rsync -avr -e "ssh ${SSHARGS}" --exclude=*.pyc --exclude=__pycache__ --exclude=venv/* ../ ${SSHUSER}@${SSHHOST}:${WEBROOT}
+rsync -avr -e "ssh ${SSHARGS}" --exclude=*.pyc --exclude=__pycache__ --exclude=venv/* --exclude=.git* ../ ${SSHUSER}@${SSHHOST}:${WEBROOT}
 
 # Build virtualenv
 ssh ${SSHARGS} ${SSHUSER}@${SSHHOST} "cd ${WEBROOT} && python3 -m virtualenv venv && venv/bin/pip3 install -r web/requirements.txt"
 
 # Migrate database
 ssh ${SSHARGS} ${SSHUSER}@${SSHHOST} "cd ${WEBROOT}/web/cloudstrype && ../../venv/bin/python3 manage.py migrate"
+
+# Collect static files
+ssh ${SSHARGS} ${SSHUSER}@${SSHHOST} "cd ${WEBROOT}/web/cloudstrype && ../../venv/bin/python3 manage.py collectstatic --noinput"
 
 # Copy config files.
 ssh ${SSHARGS} ${SSHUSER}@${SSHHOST} "sudo mv ${WEBROOT}/deploy/nginx-cloudstrype.conf ${CONFIG_NGINX}"
