49c38a2291d23d39b13fe8e1fc7a5a46e8740e37
Ben Timby <btimby@smartfile.com>
2017-02-26 17:46:07 -0500

    Working deploy. See how Travis likes it...

---
+++ deploy/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+SSHUSER="deploy"
+SSHHOST="cloudstrype.io"
+SSHARGS="-i .ssh/deploy-id_rsa"
+WEBROOT="/usr/share/nginx/cloudstrype/"
+
+CONFIG_NGINX="/etc/nginx/conf.d/cloudstrype.conf"
+CONFIG_SUPERVISORD="/etc/supervisord.d/cloudstrype.ini"
+
+rsync -avr -e "ssh ${SSHARGS}" --exclude=*.pyc --exclude=__pycache__ --exclude=venv/* ../ ${SSHUSER}@${SSHHOST}:${WEBROOT}
+
+# Build virtualenv
+ssh ${SSHARGS} ${SSHUSER}@${SSHHOST} "cd ${WEBROOT} && python3 -m virtualenv venv && venv/bin/pip3 install -r web/requirements.txt"
+
+# Migrate database
+ssh ${SSHARGS} ${SSHUSER}@${SSHHOST} "cd ${WEBROOT}/web/cloudstrype && ../../venv/bin/python3 manage.py migrate"
+
+# Copy config files.
+ssh ${SSHARGS} ${SSHUSER}@${SSHHOST} "sudo mv ${WEBROOT}/deploy/nginx-cloudstrype.conf ${CONFIG_NGINX}"
+ssh ${SSHARGS} ${SSHUSER}@${SSHHOST} "sudo mv ${WEBROOT}/deploy/supervisord-uwsgi.ini ${CONFIG_SUPERVISORD}"
+
+# Restart serices
+ssh ${SSHARGS} ${SSHUSER}@${SSHHOST} "sudo systemctl restart nginx && sudo systemctl restart supervisord"
