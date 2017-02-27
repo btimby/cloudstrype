806c70ebbb25d66cf4772597e16cd29edb88a32e
Ben Timby <btimby@smartfile.com>
2017-02-27 00:18:01 -0500

    Updated deploy script.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,23 +5,36 @@ SSHHOST="cloudstrype.io"
 SSHARGS="-i .ssh/deploy-id_rsa -oStrictHostKeyChecking=no"
 WEBROOT="/usr/share/nginx/cloudstrype/"
 
+SSHCMD="$SSHCMD"
+
 CONFIG_NGINX="/etc/nginx/conf.d/cloudstrype.conf"
 CONFIG_SUPERVISORD="/etc/supervisord.d/cloudstrype.ini"
 
-rsync -avr -e "ssh ${SSHARGS}" --exclude=*.pyc --exclude=__pycache__ --exclude=venv/* --exclude=.git* ../ ${SSHUSER}@${SSHHOST}:${WEBROOT}
+# This script is run from inside deploy/, so we have to use .. to refer to
+# local paths.
+
+rsync -avr --del -e "ssh ${SSHARGS}" --exclude-from=rsync.excludes ../ ${SSHUSER}@${SSHHOST}:${WEBROOT}
 
 # Build virtualenv
-ssh ${SSHARGS} ${SSHUSER}@${SSHHOST} "cd ${WEBROOT} && python3 -m virtualenv venv && venv/bin/pip3 install -r web/requirements.txt"
+${SSHCMD} "cd ${WEBROOT} && python3 -m virtualenv venv && venv/bin/pip3 install -r web/requirements.txt"
 
 # Migrate database
-ssh ${SSHARGS} ${SSHUSER}@${SSHHOST} "cd ${WEBROOT}/web/cloudstrype && ../../venv/bin/python3 manage.py migrate"
+${SSHCMD} "cd ${WEBROOT}/web/cloudstrype && ../../venv/bin/python3 manage.py migrate"
 
 # Collect static files
-ssh ${SSHARGS} ${SSHUSER}@${SSHHOST} "cd ${WEBROOT}/web/cloudstrype && ../../venv/bin/python3 manage.py collectstatic --noinput"
+${SSHCMD} "cd ${WEBROOT}/web/cloudstrype && ../../venv/bin/python3 manage.py collectstatic --noinput"
 
 # Copy config files.
-ssh ${SSHARGS} ${SSHUSER}@${SSHHOST} "sudo mv ${WEBROOT}/deploy/nginx-cloudstrype.conf ${CONFIG_NGINX}"
-ssh ${SSHARGS} ${SSHUSER}@${SSHHOST} "sudo mv ${WEBROOT}/deploy/supervisord-uwsgi.ini ${CONFIG_SUPERVISORD}"
+${SSHCMD} "sudo mv ${WEBROOT}/deploy/nginx-cloudstrype.conf ${CONFIG_NGINX}"
+${SSHCMD} "sudo mv ${WEBROOT}/deploy/supervisord-uwsgi.ini ${CONFIG_SUPERVISORD}"
 
 # Restart serices
-ssh ${SSHARGS} ${SSHUSER}@${SSHHOST} "sudo systemctl restart nginx && sudo systemctl restart supervisord"
+${SSHCMD} "sudo nginx -t -c /etc/nginx/nginx.conf"
+if [ $? -ne "0" ]; then
+    echo "ERROR: nginx config file has errors, aborting restart."
+    exit 1;
+fi
+
+# I don't think supervisord can check it's config before reload.
+
+${SSHCMD} "sudo systemctl restart supervisord && sudo systemctl restart nginx"
