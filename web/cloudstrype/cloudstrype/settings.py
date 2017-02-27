791da37ee6d3508afd0b36442148e3b74a8a56d9
Ben Timby <btimby@smartfile.com>
2017-02-27 01:31:08 -0500

    Don't link development version to a commit.

--- web/cloudstrype/cloudstrype/settings.py
+++ web/cloudstrype/cloudstrype/settings.py
@@ -23,7 +23,7 @@ ENV = environ.Env()
 environ.Env.read_env(pathjoin(str(ROOT), '.env'))
 environ.Env.read_env(pathjoin(str(ROOT), '.env-version'))
 
-CLOUDSTRYPE_VERSION = ENV('CLOUDSTRYPE_VERSION', default='dev')
+CLOUDSTRYPE_VERSION = ENV('CLOUDSTRYPE_VERSION', default='development')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
