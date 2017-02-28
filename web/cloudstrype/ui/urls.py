61fcb9a6de14befe8ebc03523574a965150f5d13
Ben Timby <btimby@smartfile.com>
2017-02-27 23:08:56 -0500

    New pages.

--- web/cloudstrype/ui/urls.py
+++ web/cloudstrype/ui/urls.py
@@ -7,6 +7,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='ui/index.html')),
     url(r'^start/$', TemplateView.as_view(template_name='ui/start.html')),
+    url(r'^login/$', TemplateView.as_view(template_name='ui/login.html')),
 ]
 
 if settings.DEBUG:
